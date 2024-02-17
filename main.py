2902751ada7c747d67c33f9bc9a4f902ab4bf0d8
Zitrul <63922553+Zitrul@users.noreply.github.com>
2024-02-17 22:14:42 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+from typing import Union
+import uvicorn
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_main():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
