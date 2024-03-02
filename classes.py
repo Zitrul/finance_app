760e8d67d9bafc0dcec5b68ea2f6232375c39f26
Zitrul <63922553+Zitrul@users.noreply.github.com>
2024-03-02 19:32:05 +0300

    Update classes.py
    добавил класс продукта

--- classes.py
+++ classes.py
@@ -2,3 +2,11 @@ from fastapi import UploadFile
 from pydantic import BaseModel
 class stringa(BaseModel):
     name: str
+
+class Product:
+    def __init__(self, name, price, quantity, cost, category):
+        self.price = price
+        self.quantity = quantity
+        self.cost = cost
+        self.category = category
+        self.name = name
