327ee1e6d82e5936f5346cbb3480ac67c4021af0
Zitrul <63922553+Zitrul@users.noreply.github.com>
2024-03-03 18:34:14 +0000

    Update main.py
    sdf

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ from typing import Union
 import uvicorn
 from fastapi import FastAPI, UploadFile, File
 from fastapi import UploadFile, File, Form
-
+import base64
+from io import BytesIO
+from PIL import Image
 from DBmanager import DBmanager
 from functions import CHECK_CHECKER, auto_sort
 from classes import stringa, Product
@@ -11,11 +13,8 @@ from classes import stringa, Product
 app = FastAPI()
 
 
-@app.post("/uploadfile/")
-async def create_upload_file(file: UploadFile):
-    return {"filename": file.filename}
 @app.get("/")
-def read_main(name : stringa):
+def read_main(name : str):
     print(name)
     return {"Hello": "World"}
 @app.get("/add_product_user")
@@ -31,13 +30,35 @@ def add_product(user_id: str, name: str, amount: str, category: str, currency: s
     db.commit()
     print(name, user_id)
     return {"OK": "OK"}
-
+@app.post("/uploadfile/")
+async def create_upload_file(data: str):
+    print(data)
+    with open('image.jpeg', "wb+") as file_object:
+        file_object.write(data.encode('utf-8'))
+    return {"filename": 'ok'}
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+@app.post("/check_bill")
+async def create_upload_file(base64_data:str, user_id: str, sort: str):
+    bytes_data = base64.b64decode(base64_data)
+    image_bytes = BytesIO(bytes_data)
+    image = Image.open(image_bytes)
+    file_location = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")+'.png'
+    image.save(file_location)
+    print(uploaded_file)
+    user_id = int(user_id)
+    if sort == "True":
+        sort = True
+    else:
+        sort = False
+
+    result = CHECK_CHECKER(sort, user_id, file_location)
+    return {"info": result}
 @app.post("/check_check")
 async def create_upload_file(uploaded_file: UploadFile = File(), user_id: str = Form(), sort: str = Form()):
+    print(uploaded_file)
     user_id = int(user_id)
     if sort == "True":
         sort = True
@@ -52,4 +73,4 @@ async def create_upload_file(uploaded_file: UploadFile = File(), user_id: str = 
     result = CHECK_CHECKER(sort, user_id, file_location)
     return {"info": result}
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=3214)
