9490de6de48cba85fafbe751070553401f843610
Zitrul <63922553+Zitrul@users.noreply.github.com>
2024-03-03 18:56:01 +0300

    Update classes.py

--- classes.py
+++ classes.py
@@ -4,9 +4,8 @@ class stringa(BaseModel):
     name: str
 
 class Product:
-    def __init__(self, name, price, quantity, cost, category):
-        self.price = price
-        self.quantity = quantity
-        self.cost = cost
+    def __init__(self, name, amount, currency, category):
+        self.price = amount
+        self.currency = currency
         self.category = category
         self.name = name
