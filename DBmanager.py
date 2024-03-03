0945cc1ce4bad72fbbc0830f264a8b9bbe96d346
Zitrul <63922553+Zitrul@users.noreply.github.com>
2024-03-03 09:08:15 +0300

    Update DBmanager.py
    db update

--- DBmanager.py
+++ DBmanager.py
@@ -1,6 +1,6 @@
 import pymysql.cursors
 class DBmanager:
-    def init(self):
+    def __init__(self):
 
         self.conn = pymysql.connect(host="188.244.45.227",
                        port = 3306,
@@ -12,11 +12,10 @@ class DBmanager:
         self.cur = self.conn.cursor()
 
     def add_products(self, products_list, user_id):
-        for product_name in products_list:
-            product = products_list[product_name]
+        for product in products_list:
             print(product)
-            sql = "INSERT INTO transactions (description, type, amount, currency, sum, user_id) VALUES (%s,%s, %s,%s, %s, %s)"
-            val = (product_name, product["category"], float(product["quantity"]), str(product["price"]), str(product["cost"]), user_id)
+            sql = "INSERT INTO Transaction (description, type, amount, currency, user_id) VALUES (%s,%s, %s, %s, %s)"
+            val = (str(product.name), str(product.category), float(product.quantity), str(product.cost), user_id)
             self.cur.execute(sql,val)
 
         return "OK"
