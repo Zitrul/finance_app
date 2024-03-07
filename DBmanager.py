a07942a7b1fc26442a6981dfe4a346a9b92f41d0
Zitrul <63922553+Zitrul@users.noreply.github.com>
2024-03-07 21:21:21 +0300

    Update DBmanager.py

--- DBmanager.py
+++ DBmanager.py
@@ -1,25 +1,35 @@
-import datetime
-
 import pymysql.cursors
+
+
 class DBmanager:
-    def __init__(self):
+    def init(self):
 
         self.conn = pymysql.connect(host="188.244.45.227",
-                       port = 3306,
-                       user="sigma",
-                       password="j$sdjk!53",
-                       db="main_db",
-                       charset='utf8',
-                       )
+                                    port=3306,
+                                    user="sigma",
+                                    password="j$sdjk!53",
+                                    db="main_db",
+                                    charset='utf8',
+                                    )
         self.cur = self.conn.cursor()
 
     def add_products(self, products_list, user_id):
-        for product in products_list:
+        for product_name in products_list:
+            product = products_list[product_name]
             print(product)
-            sql = "INSERT INTO Transaction (name, category, amount, currency, user_id, created_at, updated_at) VALUES (%s,%s, %s, %s, %s, %s,%s)"
-            val = (str(product.name), str(product.category), str(product.price), str(product.currency), user_id, datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S"),datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S") )
-            self.cur.execute(sql,val)
+            sql = "INSERT INTO Transaction (description, type, amount, currency, sum, user_id) VALUES (%s,%s, %s,%s, %s, %s)"
+            val = (
+            product_name, product["category"], float(product["quantity"]), str(product["price"]), str(product["cost"]),
+            user_id)
+            self.cur.execute(sql, val)
 
         return "OK"
+
+    def add_news(self, company_name, link, description, created_at):
+        sql = "INSERT INTO LatestNews (company_name, link, description, created_at) VALUES (%s, %s, %s,  %s)"
+        val = (company_name, link, description, created_at)
+        self.cur.execute(sql, val)
+        return "OK"
+
     def commit(self):
         self.conn.commit()
