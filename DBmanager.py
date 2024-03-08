cd8db17a7f3f440004b3a8849e2877e41e61e4cb
Zitrul <63922553+Zitrul@users.noreply.github.com>
2024-03-08 10:35:54 +0300

    Update DBmanager.py

--- DBmanager.py
+++ DBmanager.py
@@ -2,7 +2,7 @@ import pymysql.cursors
 
 
 class DBmanager:
-    def init(self):
+    def __init__(self):
 
         self.conn = pymysql.connect(host="188.244.45.227",
                                     port=3306,
@@ -26,10 +26,24 @@ class DBmanager:
         return "OK"
 
     def add_news(self, company_name, link, description, created_at):
-        sql = "INSERT INTO LatestNews (company_name, link, description, created_at) VALUES (%s, %s, %s,  %s)"
-        val = (company_name, link, description, created_at)
+        sql = "INSERT INTO LatestNews (company_name, link, description, created_at, send_in_telegram) VALUES (%s, %s, %s,  %s, %s)"
+        val = (company_name, link, description, created_at, "false")
         self.cur.execute(sql, val)
         return "OK"
+    def search_for_news(self):
+        sql = "SELECT * FROM LatestNews WHERE send_in_telegram = false;"
+        self.cur.execute(sql)
+        news = self.cur.fetchall()
+        return news
+    def get_link_news(self):
+        sql = "SELECT link FROM LatestNews;"
+        self.cur.execute(sql)
+        links = self.cur.fetchall()
+        return links
+    def update_news(self, list_to_update):
+        for elm in list_to_update:
+            sql = f"UPDATE LatestNews SET send_in_telegram=true WHERE id={int(elm)};"
+            self.cur.execute(sql)
 
     def commit(self):
         self.conn.commit()
